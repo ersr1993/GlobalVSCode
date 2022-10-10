8a5d28917f8c6798c3a76ac5e5f6791e3e5ef644
erasc93 <108055156+erasc93@users.noreply.github.com>
2022-10-10 18:50:12 +0200

    Update ConsDisplayer.cs

---
+++ Logic/ConsDisplayer.cs
@@ -0,0 +1,61 @@
+﻿namespace MyConsMenu
+{
+
+    using System;
+    public static class MyConsole
+    {
+        public static ConsoleKey DisplayPage(IPage somePage)
+        {
+
+            Console.Clear();
+            Console.Title = somePage.title;
+            MyWriteLine(somePage.title);
+
+            AddDottedLines();
+            MyWriteLine(somePage.content);
+            AddDottedLines();
+            
+            MyWriteLine( $"{somePage.footer} \n \n \n  ",ConsoleColor.DarkMagenta); // Je l'aime Bien
+            MyWriteLine( $"{MsgOut.FooterMsg()} ",ConsoleColor.DarkCyan );
+            //-
+            ConsoleKeyInfo myChar;
+            myChar = Console.ReadKey();
+            return myChar.Key;
+        }
+        private static void MyWriteLine(string someStr, ConsoleColor myColor=ConsoleColor.White)
+        {
+            Console.ForegroundColor = myColor;
+
+            Console.WriteLine(someStr,myColor);
+
+            Console.ResetColor();
+        }
+        private static void AddDottedLines()
+        {
+            MyWriteLine("--------------\n ", ConsoleColor.DarkYellow);
+        }
+        public static void Say(string message)
+        {
+            MyWriteLine(message);
+        }
+        public static void SayAndWait(string message)
+        {
+            Console.WriteLine("\n Output : \n" + message);
+            Console.ReadKey();
+        }
+        public static string AskTypeLine(string question)
+        {
+            string typedKey;
+            //-
+            MyWriteLine(question,ConsoleColor.DarkYellow);
+            typedKey = Console.ReadLine();
+            //-
+            return typedKey;
+        }
+        public static string OtherString()
+        {
+            return "otherString";
+        }
+    }
+}
+
